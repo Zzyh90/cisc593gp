d4247ef4f33a28efd4167820d7e28f9a021a6b72
Ugur Yilmaz <uguryiilmz95@gmail.com>
2023-01-26 23:50:35 -0500

    new branch password hash and salting

--- back/tests/createUsers.test.js
+++ back/tests/createUsers.test.js
@@ -2,6 +2,8 @@ const users= require('../dao/users')
 const Users = require ('../model/Users')
 
 const db= require('./db')
+const bcrypt = require("bcrypt")
+
 
 beforeAll(async () => await db.connect())
 
@@ -10,7 +12,7 @@ afterEach(async () => await db.clearDatabase())
 afterAll(async () => await db.closeDatabase())
 
 describe("user creation", ()=>{
-    it('create user', async () =>{
+    it('create patient user', async () =>{
         const {userId}= await users.addUser('ugur','yilmaz','ugur@gmail.com','12345',false)
 
 
@@ -19,8 +21,54 @@ describe("user creation", ()=>{
         expect(user.firstName).toEqual("ugur")
         expect(user.lastName).toEqual("yilmaz")
         expect(user.email).toEqual("ugur@gmail.com")
-        expect(user.passwordHash).toEqual("12345")
         expect(user.isDoctor).toEqual(false)
+
+    })
+
+    it('create doctor user', async () =>{
+        const {userId}= await users.addUser('doctor','michael','michael@gmail.com','12345',true)
+
+
+        const user = await Users.findById(userId)
+
+
+        expect(user.firstName).toEqual("doctor")
+        expect(user.lastName).toEqual("michael")
+        expect(user.email).toEqual("michael@gmail.com")
+        expect(user.isDoctor).toEqual(true)
+
+    })
+
+
+    it('user has the right password secured', async () =>{
+        const {userId}= await users.addUser('mike','ike','ike@gmail.com','testpassword1!',true)
+
+
+        const user = await Users.findById(userId)
+
+        const validPassword= await bcrypt.compare('testpassword1!', user.password)
+
+
+        expect(validPassword).toEqual(true)
+
+        //because its salted and hashed in db
+        expect(user.password).not.toEqual('testpassword1!')
+
+    })
+
+    it('user has the right password secured', async () =>{
+        const {userId}= await users.addUser('mike','ike','ike@gmail.com','testpassword1!',true)
+
+
+        const user = await Users.findById(userId)
+
+        const validPassword= await bcrypt.compare('testpassword1!', user.password)
+
+
+        expect(validPassword).toEqual(true)
+
+        //because its salted and hashed in db
+        expect(user.password).not.toEqual('testpassword1!')
 
     })
 })
